4f2ad96839e301f652c1174a26c4dcc02c4f4ea2
Virginie Grosboillot <vestalisvirginis@gmail.com>
2024-02-29 13:01:26 +0000

    test search tool

--- app/llm-test.py
+++ app/llm-test.py
@@ -5,5 +5,6 @@ from agents import toxpipe as tp
 
 tpa = tp.ToxPipeAgent(model="gpt-4", api_version="2024-02-15-preview", temp=0.1, max_iterations=40)
 #res = tpa.run("What is the molecular weight of tylenol? What about for aspirin?")
-res = tpa.run("What is the molecular weight of aspirin?")
+#res = tpa.run("What is the molecular weight of aspirin?")
+res = tpa.run("What are the mechanism of action of aspirin?") # to test search tool
 print(res)
